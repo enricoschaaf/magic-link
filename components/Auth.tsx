d44575c08991f3b8b76d7bc8c104fa61ab82b26e
enricoschaaf <enricoschaaf98@gmail.com>
2020-06-07 10:30:22 +0200

    delete Auth component

--- components/Auth.tsx
+++
@@ -1,9 +0,0 @@
-import { useUser } from "hooks/useUser"
-import { UserContext } from "utils/Context"
-
-const Auth = ({ children }) => {
-  const user = useUser()
-  return <UserContext.Provider value={user}>{children}</UserContext.Provider>
-}
-
-export default Auth
